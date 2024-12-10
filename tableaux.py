0d0751d0a2cf91e80930e35b4cd1b4f6cdf5730b
LEFEBVRE Pierre <106326833+FazeNextuss@users.noreply.github.com>
2024-12-10 09:49:32 +0100

    Add assertions

--- tableaux.py
+++ tableaux.py
@@ -6,23 +6,25 @@ k = [i for i in range(2,13,2)]
 l = [[0 for i in range(3)], [1 for i in range(3)], [2 for i in range(3)]]
 m = [[j for i in range(3)] for j in range(3)]
 n = [i * 3 - 6 for i in range(4)]
-
-# [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 o = [[i+j for j in range(3)] for i in range(1,10,3)]
 
 
 def generer_tab_croissant(n):
-    if n > 0:
-        tab = [i for i in range(n)]
+    assert n > 0, "n n'est pas positif"
+    
+    tab = [i for i in range(n)]
     return tab
 
 def generer_tab_decroissant(n):
-    if n > 0:
-        tab = [i for i in range(n-1, -1, -1)]
-        return tab
+    assert n > 0, "n n'est pas positif"
+    
+    tab = [i for i in range(n-1, -1, -1)]
+    return tab
     
 def generer_tab_melange(n):
-    if n > 0:
-        i =  [randint(0,100) for i in range(n)]
-        return i
+    assert n > 0, "n n'est pas positif"
+    
+    i =  [randint(0,100) for i in range(n)]
+    return i
         
+
